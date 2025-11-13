c5b16c2607c6296a641ba166a3df20f95006118a
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-13 20:44:00 +0000

    Add OrderScreen and its state management for quantity handling

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,25 @@ void main() {
   // runApp(const MyApp());
   runApp(const App());
 }
+class OrderScreen extends StatefulWidget {
+  final int maxQuantity;
 
+  const OrderScreen({super.key, this.maxQuantity = 10});
+
+  @override
+  State<OrderScreen> createState() {
+    return _OrderScreenState();
+  }
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  int _quantity = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 class OrderItemDisplay extends StatelessWidget {
   final String itemType;
   final int quantity;
