3b11d40d3847e65256275c7c80742a45e2b834b9
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-11-20 05:12:11 +0000

    Add OrderRepository class to manage sandwich order quantities

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:sandwich_shop/repositories/order_repository.dart';
 
 void main() {
   // runApp(const MyApp());
