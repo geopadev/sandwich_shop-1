cf6fc241092463c3d6615e296ec0f21ee311ea5f
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-12-12 02:48:06 +0000

    Refactor CheckoutScreen to use Provider for Cart management and update profile screen imports

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:provider/provider.dart';
+import 'package:sandwich_shop/models/cart.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
